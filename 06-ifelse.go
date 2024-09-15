c6d2bc9a761629e661387aaee471afd4a7ab7a73
Kr1shnam00rthi <p.l.krishnamoorthy@gmail.com>
2024-09-15 22:29:25 +0530

    basics

---
+++ 06-ifelse.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var number int =5
+	
+	if number%2==0{
+		fmt.Println("Number is even")
+	} else {
+		fmt.Println("Number is odd")
+	}
+	
+	if num:=9; num<0{
+		fmt.Println(num,"is negative")
+	} else if num<10{
+		fmt.Println(num,"has 1 digit")
+	} else{
+		fmt.Println(num, "has multiple digits")
+	}
+} 
+	
